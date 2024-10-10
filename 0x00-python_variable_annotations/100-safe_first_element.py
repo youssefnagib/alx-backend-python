a6f5b426ecd96bac61f654a480bf31148ac01bd9
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 20:09:24 +0300

    annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Module safe_first_element
+'''
+from typing import Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    if lst:
+        return lst[0]
+    else:
+        return None
