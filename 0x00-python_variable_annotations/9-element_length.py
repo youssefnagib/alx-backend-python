8fde37ec826451b60745a4b0d44727368542725c
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 20:04:58 +0300

    annotations

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 '''
-    This module contains a function to get the length of each element in a list.
+    This module contains a function
+    to get the length of each element in a list.
 '''
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     '''
-    Function element_length it returns a list of tuples, where each tuple contains the index and length of an element in the input list.
+    Function element_length it returns a list of tuples, where each tuple contains
+    the index and length of an element in the input list.
     '''
     return [(i, len(i)) for i in lst]
