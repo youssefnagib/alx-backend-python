6354dbc5ee04cb75cb4a719116131fbad6715065
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 19:23:12 +0300

    sum list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    This module contains a function to return the sum of all numbers in a list.
+'''
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    '''
+    function sum_mixed_list it sum all in list
+    Args:
+        mxd_lst (list): A list of integers and/or floats.
+    Returns:
+        float: The sum of all numbers in the list.
+    '''
+    return sum(mxd_lst)
+
