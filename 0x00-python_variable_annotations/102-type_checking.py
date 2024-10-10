b6fea3a3f926c8d8b453a7f3cd31510fbdaa8c23
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 20:39:24 +0300

    annotations

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Module zoom_array
+'''
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''
+    Function zoom_array returns a new tuple containing the elements
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
