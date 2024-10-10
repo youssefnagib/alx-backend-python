c92c56012d9cae9964dfabcba714634bb2217530
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 14:18:10 +0300

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    This module contains a function to convert a float to a string.
+'''
+
+
+def to_str(n:float) -> str:
+    """
+    Convert a float to a string.
+
+    Args:
+        n (float): The float to convert.
+
+    Returns:
+        str: The converted float as a string.
+    """
+    return str(n)
