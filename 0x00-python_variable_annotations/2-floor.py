ee400d16ca45b54b3f4c2dc7b11c2b3302eac0db
youssef nagib <youssefnagib14@yahoo.com>
2024-10-10 14:15:06 +0300

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+    This module contains a function to return the floor of a float.
+'''
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The float to floor.
+
+    Returns:
+        float: The floor of n.
+    """
+    return int(n)
