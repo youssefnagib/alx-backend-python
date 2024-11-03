91db225ec72c3fb9f5eb4224fc012355dcf596b9
youssef nagib <youssefnagib14@yahoo.com>
2024-11-03 19:52:32 +0200

    test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,21 +54,22 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
-    ''' test memoize function'''
-    class TestClass:
+    """Tests the `memoize` function."""
+    def test_memoize(self):
+        """Tests `memoize`'s output."""
+        class TestClass:
+            def a_method(self):
+                return 42
 
-        def a_method(self):
-            return 42
-
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-    @patch('__main__.TestMemoize.TestClass.a_method')
-    def test_memoize(self, mock):
-        '''Test memoize function'''
-        mock.return_value = 42
-        inst = self.TestClass()
-        self.assertEqual(inst.a_property, 42)
-        self.assertEqual(inst.a_property, 42)
-        mock.assert_called_once()
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+                ) as mock:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            mock.assert_called_once()
